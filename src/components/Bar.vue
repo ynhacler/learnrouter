2278121aad20e53fa13bbe0bee7009aae4886db7
git <zzh1989@126.com>
2018-12-20 15:54:46 +0800

    first commit

---
+++ src/components/Bar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bar">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Bar',
+  data () {
+    return {
+      msg: 'Bar2222222222222222222'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  background: blue;
+  color: white;
+}
+</style>
---
+++ src/components/Foo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="foo">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Foo',
+  data () {
+    return {
+      msg: 'Foo11111111111'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  background: black;
+  color: white;
+}
+</style>
